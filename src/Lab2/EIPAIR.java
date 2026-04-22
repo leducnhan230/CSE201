c0a25fcbda3bea16d9f500989f55b55e80fb4004
Le Duc Nhan <leducnhan230@gmail.com>
2026-04-22 08:51:04 +0700

    refactor: update to a better approach for EIPAIR

--- src/Lab2/EIPAIR.java
+++ src/Lab2/EIPAIR.java
@@ -1,46 +1,31 @@
 package Lab2;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class EIPAIR {
-
-    public static long countTypes(long n, long[] arr) {
-        long count = 0;
-        for (long t : arr)
-            if (t == n)
-                count++;
-        return count;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
-
         int testCases = sc.nextInt();
-        for (long i = 0; i < testCases; i++) {
 
-            // Input:
-            int arrLength = sc.nextInt();
-            long arr[] = new long[arrLength];
-            for (int j = 0; j < arr.length; j++) {
-                arr[j] = sc.nextLong();
+        while (testCases-- > 0) {
+            int gifts = sc.nextInt();
+            Map<Long, Long> priceMap = new HashMap<>();
+
+            for (int i = 0; i < gifts; i++) {
+                long price = sc.nextLong();
+                priceMap.put(price, priceMap.getOrDefault(price, 0L) + 1);
             }
 
-            Arrays.sort(arr);
-
-            // Calculation:
-            long numberChoices = 0;
-            for (int j = 0; j < arr.length - 1; j++) {
-
-                long countTypes = 0;
-                if (arr[j] == arr[j + 1]) {
-                    countTypes = countTypes(arr[j], arr);
-                    j += countTypes - 1;
+            long totalChoices = 0;
+            for (long count : priceMap.values()) {
+                if (count >= 2) {
+                    totalChoices += count * (count - 1) / 2;
                 }
-                numberChoices += countTypes * (countTypes - 1) / 2;
             }
-            sb.append(numberChoices).append(System.lineSeparator());
+            sb.append(totalChoices).append(System.lineSeparator());
         }
         System.out.println(sb);
         sc.close();
