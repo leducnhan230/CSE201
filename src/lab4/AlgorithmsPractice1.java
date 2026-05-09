86eb3f9848ca93d54379844b9381ac8112554f62
Le Duc Nhan <leducnhan230@gmail.com>
2026-05-09 14:12:48 +0700

    refactor(lab4): rearrange methods and improve variable naming inAlgorithmsPractice1

--- src/lab4/AlgorithmsPractice1.java
+++ src/lab4/AlgorithmsPractice1.java
@@ -3,58 +3,70 @@ package lab4;
 import java.util.Scanner;
 
 public class AlgorithmsPractice1 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
+    public static Scanner sc = new Scanner(System.in);
+    public static StringBuilder sb = new StringBuilder();
 
+    // Binary Search
+    public static int BinarySearch(int[] arr) {
+        int target = sc.nextInt();
+
+        int low = 0, high = arr.length - 1, ans = -1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] >= target) {
+                if (arr[mid] == target)
+                    ans = mid;
+                high = mid - 1;
+
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    // Sliding Window
+    public static long SlidingWindow(int[] arr) {
+        int windowSize = sc.nextInt();
+
+        long min = Long.MAX_VALUE;
+        long temp = 0;
+        for (int i = 0; i <= arr.length - windowSize; i++) {
+            if (i == 0) {
+                for (int j = 0; j < windowSize; j++) {
+                    temp += arr[j];
+                }
+
+                if (temp < min)
+                    min = temp;
+
+            } else {
+                temp = temp - arr[i - 1] + arr[(windowSize - 1) + i];
+                if (temp < min)
+                    min = temp;
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
         String name = sc.next();
-        int arrLength = sc.nextInt();
+        int length = sc.nextInt();
         int queries = sc.nextInt();
 
-        int[] arr = new int[arrLength];
-        for (int i = 0; i < arr.length; i++) {
+        // Array input:
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++) {
             arr[i] = sc.nextInt();
         }
 
-        int[] PS = new int[arrLength];
-        PS[0] = arr[0];
-        for (int i = 1; i < PS.length; i++) {
-            PS[i] = PS[i - 1] + arr[i];
-        }
-
-        while (queries-- > 0) {
-            if (name.equals("BinarySearch")) {
-                int target = sc.nextInt();
-
-                int low = 0, high = arrLength - 1, ans = -1;
-                while (low <= high) {
-                    int mid = low + (high - low) / 2;
-                    if (arr[mid] >= target) {
-                        if (arr[mid] == target)
-                            ans = mid;
-                        high = mid - 1;
-                    } else {
-                        low = mid + 1;
-                    }
-                }
-                sb.append(ans).append(System.lineSeparator());
-            
-            } else {
-                int subArray = sc.nextInt();
-
-                // Sliding Window
-                long min = PS[subArray - 1];
-
-                long minSum = min;
-                for (int i = 1; i <= arr.length - subArray; i++) {
-                    min = min - arr[i - 1] + arr[(subArray - 1) + i];
-                    if (min < minSum) {
-                        minSum = min;
-                    }
-                }
-                sb.append(minSum).append(System.lineSeparator());
-            }
-        }
+        if (name.equals("BinarySearch"))
+            while (queries-- > 0)
+                sb.append(BinarySearch(arr)).append(System.lineSeparator());
+        else
+            while (queries-- > 0)
+                sb.append(SlidingWindow(arr)).append(System.lineSeparator());
         System.out.println(sb);
         sc.close();
     }
