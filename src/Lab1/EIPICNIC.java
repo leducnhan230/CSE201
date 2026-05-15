fbfca87f68e9aadfe0ba4ce69b6a69ca8cd6df7e
Le Duc Nhan <leducnhan230@gmail.com>
2026-05-15 19:50:34 +0700

    refactor(EIPICNIC): update a better approach for EIPICNIC

--- src/Lab1/EIPICNIC.java
+++ src/Lab1/EIPICNIC.java
@@ -5,38 +5,19 @@ import java.util.Scanner;
 public class EIPICNIC {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-        int numberGroups = sc.nextInt();
-        int[] groups = new int[5];
-        for (int i = 0; i < numberGroups; i++) {
-            byte input = sc.nextByte();
-            groups[input]++;
+        int[] arr = new int[5];
+        for (int i = 0; i < n; i++) {
+            int input = sc.nextInt();
+            arr[input]++;
         }
 
-        int numberCars = 0;
+        arr[1] = Math.max(0, arr[1] - (arr[3] + (arr[2] % 2) * 2));
 
-        // group 4:
-        numberCars += groups[4];
+        long total = arr[4] + arr[3] + arr[2] / 2 + arr[2] % 2 + (long) Math.ceil(arr[1] / 4.0);
 
-        // group 3 + group 1:
-        int remainingG1 = 0;
-        if (groups[3] >= groups[1]) {
-            numberCars += groups[1]; // number of cars is calculate by making group 1 and 3 together.
-            numberCars += groups[3] - groups[1]; // number of cars for remaining group 3.
-
-        } else {
-            numberCars += groups[3];
-            remainingG1 = groups[1] - groups[3];
-        }
-
-        // group 2:
-        numberCars += groups[2] / 2;
-        int remainingG2 = groups[2] % 2;
-
-        double temp = (remainingG1 + remainingG2 * 2) / 4.0;
-        numberCars += Math.ceil(temp);
-
-        System.out.println(numberCars);
+        System.out.println(total);
         sc.close();
     }
 }
