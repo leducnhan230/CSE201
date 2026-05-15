295a23366f45de944b740ddd762c77a8ffd243cd
Le Duc Nhan <leducnhan230@gmail.com>
2026-05-15 19:49:02 +0700

    refactor(EIPAGES): update a better approach for EIPAGES

--- src/Lab1/EIPAGES.java
+++ src/Lab1/EIPAGES.java
@@ -4,46 +4,41 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class EIPAGES {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
+        int n = sc.nextInt();
 
-        int pageListed = sc.nextInt();
-        long arr[] = new long[pageListed + 1];
-        for (int i = 0; i < pageListed; i++) {
-            arr[i] = sc.nextLong();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
 
-        arr[pageListed] = 2_000_000_000;
         Arrays.sort(arr);
 
-        sb.append(arr[0]);
-        int count = 0;
-        for (int i = 0; i < pageListed; i++) {
-            if (arr[i] + 1 == arr[i + 1]) {
+        int i = 0;
+        while (i < n) {
+            int start = arr[i];
+            int end = start;
 
-                count++;
-                if (count >= 2 && i + 1 == pageListed) {
-                    sb.append("-").append(arr[pageListed - 1]);
-                }
-
-            } else {
-                if (count >= 2) {
-                    sb.append("-").append(arr[i]);
-                } else if (count == 1) {
-                    sb.append(" ").append(arr[i]);
-                }
-
-                if (i < pageListed - 1) {
-                    sb.append(" ");
-                }
-
-                if (arr[i + 1] != arr[pageListed]) {
-                    sb.append(arr[i + 1]);
-                }
-                count = 0;
+            while (i + 1 < n && arr[i] == arr[i + 1] - 1) {
+                i++;
+                end = arr[i];
             }
+
+            if (sb.length() > 0)
+                sb.append(" ");
+
+            int length = end - start + 1;
+            if (length >= 3) {
+                sb.append(start).append("-").append(end);
+            } else if (length == 2) {
+                sb.append(start).append(" ").append(end);
+            } else {
+                sb.append(start);
+            }
+
+            i++;
         }
 
         System.out.println(sb);
